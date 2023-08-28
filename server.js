a39738d9312f9766af6c42b7bd89bd9c648810be
한상일 <hansang-il@hansang-il-ui-MacBookAir.local>
2023-08-28 15:19:15 +0900

    일부 패키지 추가

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
+/* 필요한 패키지 Import */
 import express          from 'express'
 import session          from 'express-session'
+import RateLimit        from 'express-rate-limit'
 import dotenv           from 'dotenv'
 import cors             from 'cors'
 import cookieParser     from 'cookie-parser'
+import morgan           from 'morgan'
+/* 필요한 패키지 Import */
 
-
-/* Routing */
+/* Routing - import 부분 */
 
 /* Routing */
 
 dotenv.config()
 
+const Limiter = RateLimit({
+    windowMs: 60 * 1000,
+    max: 500, // 1분당 API 요청 500회 이상일 경우
+    handler(req,res) {
+        res.status(this.statusCode).json({
+            code: this.statusCode,
+            message: "1분당 API 호출 횟수 초과!" // 얘를 출력하며 1분간 API 호출을 막음.
+        })
+    }
+})
+
 const app = express()
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined')) // 배포 환경 : log에 IP를 남김
+} else {
+    app.use(morgan('dev')) // 개발환경 에서  console.log에 log 기록
+}
+
 
 app.set('PORT', process.env.PORT || 8095)
 
